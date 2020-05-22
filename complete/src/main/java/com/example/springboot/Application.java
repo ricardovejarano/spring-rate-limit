7d38670adc96739a5ef01eefc0526908a07fc2a0
Ricardo <ricardo.vejarano.garcia@gmail.com>
2020-05-22 11:11:51 -0500

    Bucket logic no in controller

--- complete/src/main/java/com/example/springboot/Application.java
+++ complete/src/main/java/com/example/springboot/Application.java
@@ -1,15 +1,32 @@
 package com.example.springboot;
 
+import java.time.Duration;
 import java.util.Arrays;
 
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Bucket4j;
+import io.github.bucket4j.Refill;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class Application {
+public class Application implements WebMvcConfigurer {
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		Refill refill = Refill.intervally(10, Duration.ofMinutes(1));
+		Bandwidth limit = Bandwidth.classic(10, refill);
+		Bucket bucket = Bucket4j.builder().addLimit(limit).build();
+		registry.addInterceptor(new RateLimitInterceptor(bucket, 1))
+				.addPathPatterns("/");
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -1,40 +1,17 @@
 package com.example.springboot;
 
 
-import io.github.bucket4j.Bandwidth;
-import io.github.bucket4j.Bucket;
-import io.github.bucket4j.Refill;
-import io.github.bucket4j.Bucket4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.time.Duration;
-
-
 @RestController
 public class HelloController {
 
-	private final Bucket bucket;
-
-	public HelloController() {
-		long capacity = 10;
-		Refill refill = Refill.greedy(10, Duration.ofMinutes(1));
-		Bandwidth limit = Bandwidth.classic(capacity, refill);
-		this.bucket = Bucket4j.builder().addLimit(limit).build();
-
-		// OR
-		// Bandwidth limit = Bandwidth.simple(10, Duration.ofMinutes(1));
-	}
-
-
 	@RequestMapping("/")
 	public String index() {
-		if (this.bucket.tryConsume(1)) {
 			return "Greetings from Spring Boot!";
-		}
-		return "No se puede";
 	}
 
 	@GetMapping("/simple")
